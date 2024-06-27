fb47e3bd3d4c5132188ef8b5ce118704533d27c3
AbdullahFathan <abdullahfathan013@gmail.com>
2024-06-27 21:17:36 +0700

    UI: dashboard page done

---
+++ src/constants/dashboard.js
@@ -0,0 +1,22 @@
+export const dashBoardData = [
+  {
+    title: "Mentor Telah Approve",
+    data: 8,
+    color: "text-green",
+  },
+  {
+    title: "Lomba Telah Approve",
+    data: 10,
+    color: "text-green",
+  },
+  {
+    title: "Mentor Perlu Approve",
+    data: 2,
+    color: "text-yellow",
+  },
+  {
+    title: "Lomba Perlu Approve",
+    data: 6,
+    color: "text-yellow",
+  },
+];
--- src/features/dashboard/pages/Dashboard.jsx
+++ src/features/dashboard/pages/Dashboard.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import MainLayout from "../../../layout/MainLayout";
+import { dashBoardData } from "../../../constants/dashboard";
 
 const Dashboard = () => {
   return (
     <MainLayout>
-      <div>ini dari dashboard</div>
+      <h1 className="tilteText">Dashboard Admin</h1>
+      <div className="my-5 flex flex-col md:flex-row ">
+        {" "}
+        {dashBoardData.map((item, index) => (
+          <div
+            key={index}
+            className="flex flex-col border border-border py-3 mt-6 md:mt-0 mr-0 md:mr-6 justify-center items-center rounded-lg "
+          >
+            <h2 className={`text-2xl font-semibold  ${item.color}`}>
+              {item.data}
+            </h2>
+            <h4 className="text-base text-center text-textBlack mt-3">
+              {item.title}
+            </h4>
+          </div>
+        ))}
+      </div>
+
+      <div className="flex flex-col border border-border py-3 mt-6  justify-center items-center rounded-lg ">
+        <h2 className={`text-2xl font-semibold  text-primary`}>Rp1.504.800</h2>
+        <h4 className="text-base text-center font-normal text-textBlack mt-3">
+          Total Jumlah Pemasukan
+        </h4>
+      </div>
     </MainLayout>
   );
 };
